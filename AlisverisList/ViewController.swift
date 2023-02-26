2f5b4fc4cf83215595cc2faf457c321786b2f6b1
Ibrahim Duman <dumanibrahm@gmail.com>
2023-02-26 20:58:32 +0300

    Core Data bağlantıları yapıldı

---
+++ AlisverisList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlisverisList
+//
+//  Created by İbrahim Duman on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
