0bd72cc3eeb42b452a09abe108070d3e315515ad
Ibrahim Duman <dumanibrahm@gmail.com>
2023-03-01 19:07:35 +0300

    Silme Butonu eklendi

--- AlisverisList/DetailsViewController.swift
+++ AlisverisList/DetailsViewController.swift
@@ -6,30 +6,114 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var sizeText: UITextField!
     @IBOutlet weak var priceText: UITextField!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var imageView: UIImageView!
+    
+    var secilenUrunIsmi = ""
+    var secilenUrunUUID : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if secilenUrunIsmi != ""{
+            //Core Data seçilen ürün bilgileini göster
+            if let uuidString = secilenUrunUUID?.uuidString{
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                do{
+                    let sonuclar = try context.fetch(fetchRequest)
+                    if sonuclar.count > 0{
+                        for sonuc in sonuclar as! [NSManagedObject]{
+                            if let isim = sonuc.value(forKey: "isim") as? String{
+                                nameText.text = isim
+                            }
+                            if let fiyat = sonuc.value(forKey: "fiyat") as? Int{
+                                priceText.text = String(fiyat)
+                            }
+                            if let beden = sonuc.value(forKey: "beden") as? String{
+                                sizeText.text = beden
+                            }
+                            if let gorsel = sonuc.value(forKey: "gorsel") as? Data{
+                                let image = UIImage(data: gorsel)
+                                imageView.image = image
+                            }
+                        }
+                    }
+                }catch{
+                    print("Hata")
+                }
+            }
+        }else{
+            nameText.text = ""
+            priceText.text = ""
+            sizeText.text = ""
+            
+        }
+        
+        
 
-        // Do any additional setup after loading the view.
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(clickImage))
+        imageView.addGestureRecognizer(imageGestureRecognizer)
+        
+        
     }
     
     @IBAction func saveButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let alisveris = NSEntityDescription.insertNewObject(forEntityName: "Alisveris", into: context)
+        
+        alisveris.setValue(nameText.text, forKey: "isim")
+        alisveris.setValue(sizeText.text, forKey: "beden")
+        
+        if let fiyat = Int(priceText.text!){
+            alisveris.setValue(fiyat, forKey: "fiyat")
+        }
+        alisveris.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        alisveris.setValue(data, forKey: "gorsel")
+        do{
+            try context.save()
+            print("kaydedildi")
+        }catch{
+            print("hata")
+        }
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "veriGirildi"), object: nil)
+        self.navigationController?.popViewController(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func clickImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
     }
-    */
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+    
+    @objc func closeKeyboard(){
+        view.endEditing(true)
+    }
+    
 }
--- AlisverisList/ViewController.swift
+++ AlisverisList/ViewController.swift
@@ -6,16 +6,122 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var names = [String]()
+    var ids = [UUID]()
+    var secilenIsim = ""
+    var secilenUUID : UUID?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchData()
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(fetchData), name: NSNotification.Name(rawValue: "veriGirildi"), object: nil)
+    }
+    @objc func fetchData(){
+        
+        names.removeAll(keepingCapacity: false)
+        ids.removeAll(keepingCapacity: false)
+        
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let sonuclar = try context.fetch(fetchRequest)
+            if sonuclar.count > 0 {
+                for sonuc in sonuclar as! [NSManagedObject]{
+                    if let isim = sonuc.value(forKey: "isim") as? String{
+                        names.append(isim)
+                    }
+                    if let id = sonuc.value(forKey: "id") as? UUID{
+                        ids.append(id)
+                    }
+                }
+                
+                tableView.reloadData()
+            }
+        }catch{
+            print("hata")
+        }
+    }
+    
+    @objc func addButton(){
+        secilenIsim = ""
+        performSegue(withIdentifier: "detailVC", sender: nil)
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailVC" {
+            let destinationVC = segue.destination as! DetailsViewController
+                destinationVC.secilenUrunIsmi = secilenIsim
+                destinationVC.secilenUrunUUID = secilenUUID
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        secilenIsim = names[indexPath.row]
+        secilenUUID = ids[indexPath.row]
+        performSegue(withIdentifier: "detailVC", sender:nil)
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+            let uuidString = ids[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let sonuclar = try context.fetch(fetchRequest)
+                if sonuclar.count > 0 {
+                    for sonuc in sonuclar as! [NSManagedObject] {
+                        if let id = sonuc.value(forKey: "id") as? UUID{
+                            if id == ids[indexPath.row] {
+                                context.delete(sonuc)
+                                names.remove(at: indexPath.row)
+                                ids.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                do{
+                                    try context.save()
+                                }catch{
+                                    
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+            }catch{
+                print("Hata")
+            }
+            
+        }
+    }
 }
 
